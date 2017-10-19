4e4d77dcb4b99df277e5c6259fca1c53f255c0f6
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-10-19 11:12:45 -0400

    set up skeleton of the program prescraping ability

--- lib/celebrity_book_clubs/cli.rb
+++ lib/celebrity_book_clubs/cli.rb
@@ -1,7 +1,48 @@
-class CelebrityBookClubs::CLI 
+class CelebrityBookClubs::CLI
 
   def call
     puts "Welcome to Celebrity Book Clubs"
+    list_clubs
+    puts "Enter the number of the club to see what book or books they are reading this month."
+    list_books
   end
+
+  def list_clubs #method that will scrape club names
+    puts <<-DOC
+    1. Oprah’s Book Club
+  	2. RW Book Club
+  	3. Our Shared Shelf - Emma Watson
+  	4. Belletrist - Emma Roberts
+  	5. between two books - Florence Welch
+    DOC
+  end
+
+  def list_books
+    input = nil
+    while input != 'exit'
+    input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Oprah's books"
+      when "2"
+        puts "Reese Witherspoon's books"
+      when "3"
+        puts "Emma's Watson's books"
+      when "4"
+        puts "Emma Robert's books"
+      when "5"
+        puts "Florence Welch's books"
+      when "list"
+        list_clubs
+      when "exit"
+        read_you_later
+      end
+    end
+  end
+
+  def read_you_later
+    puts "See you again soon!"
+  end
+
 
 end
